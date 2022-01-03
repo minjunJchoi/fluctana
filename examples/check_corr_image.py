5b5cd621708b1eeeddc147e3c91111564c8a2cbe
Minjun J. Choi <mjchoi@nfri.re.kr>
2022-01-03 11:56:19 +0900

    corr image mod

--- examples/check_corr_image.py
+++ examples/check_corr_image.py
@@ -16,10 +16,8 @@ def play(shot, trange, ref, clist, flimits=[0,20], vlimits=[-0.1,0.1]):
     A.add_data(dev='KSTAR', shot=shot, clist=clist, trange=trange, norm=1)
 
     # band pass filter  # OK
-    A.filt(0,'FIR_pass',flimits[0]*1000.0,0,0.01) # smaller b is sharper
-    A.filt(0,'FIR_pass',0,flimits[1]*1000.0,0.01) # smaller b is sharper
-    A.filt(1,'FIR_pass',flimits[0]*1000.0,0,0.01) # smaller b is sharper
-    A.filt(1,'FIR_pass',0,flimits[1]*1000.0,0.01) # smaller b is sharper
+    A.filt(dnum=0,name='FFT_pass',fL=flimits[0]*1000,fH=flimits[1]*1000)
+    A.filt(dnum=1,name='FFT_pass',fL=flimits[0]*1000,fH=flimits[1]*1000)
 
     # A.iplot(dnum=0,snum=0,vlimits=[-0.1,0.1],istep=0.002,imethod='cubic',cutoff=0.03,pmethod='scatter')
     A.fftbins(nfft=512,window='hann',overlap=0.5,detrend=0,full=1)
