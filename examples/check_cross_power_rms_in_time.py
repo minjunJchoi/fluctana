5f808ba2831d739ece155961b16c631144b55730
Minjun J. Choi <mjchoi@nfri.re.kr>
2022-06-21 15:43:29 +0900

    add example for tcal

---
+++ examples/check_cross_power_rms_in_time.py
@@ -0,0 +1,29 @@
+import sys, os
+sys.path.insert(0, os.pardir)
+from fluctana import *
+
+## ===================== Calculate cross power rms ================
+shot = 10186
+trange = [15,16]
+clist1 = ['ECEI_L1303']
+clist2 = ['ECEI_L1403']
+vkind = 'cross_power'
+twin = 50e-3; tstep = 10e-3
+vpara = {'nfft':512, 'window':'hann', 'overlap':0.5, 'detrend':0, 'full':0, 'norm':1, 'f1':1000, 'f2':50000}
+
+# read not normalized data      
+A = FluctAna()
+A.add_data(dev='KSTAR', shot=shot, clist=clist1, trange=trange, norm=0)
+A.add_data(dev='KSTAR', shot=shot, clist=clist2, trange=trange, norm=0)
+
+# calculation along time 
+A.tcal(done=0, dtwo=1, twin=twin, tstep=tstep, vkind=vkind, vpara=vpara)
+
+# take result 
+taxis = A.Dlist[0].ax
+cpower = A.Dlist[0].val[0,:]
+
+# plot result 
+plt.plot(taxis, cpower)
+plt.show()
+
